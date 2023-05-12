d10e34d24dade9d7775ad80d6f06cedd8cff5e45
Shanu Kumar <shanukumar467@gmail.com>
2023-05-12 16:27:23 +0530

    Header updated

--- src/Components/Nav/index.js
+++ src/Components/Nav/index.js
@@ -1,8 +1,33 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import {navigation} from '../../data'
 
 const Nav = () => {
   return (
-    <div>Nav</div>
+    <nav>
+        <ul className='flex space-x-8 capitalize text-[15px]'>
+            {
+                navigation.map((item, i) => {
+                    return (
+                        <li key={i} className='text-white hover:text-accent cursor-pointer '>
+                            {/* <Link 
+                            to={item.href}
+                            activeClass='active'
+                            spy={true}
+                            smooth={true}
+                            duration={500}
+                            offset={-70}
+                            className='transition-all'
+                            duration-300
+                             > */}
+                                hello
+                            {/* </Link> */}
+                        </li>
+                    )
+                })
+            }
+        </ul>
+    </nav>
   )
 }
 
