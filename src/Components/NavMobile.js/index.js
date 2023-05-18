5b3172cbd3995a553811da6b08eced9087d1e65c
Shanu Kumar <shanukumar467@gmail.com>
2023-05-18 09:39:29 +0530

    mobilenav component added

--- src/Components/NavMobile.js/index.js
+++ src/Components/NavMobile.js/index.js
@@ -1,8 +1,46 @@
-import React from 'react'
+import React, { useState } from 'react'
+ import Socials from '../Socials'
+ import {motion} from 'framer-motion'
+ import { Link } from 'react-scroll'
+import {MenuAlt3Icon} from 'react-icons/me'
+
 
 const NavMobile = () => {
+  const [isOpen, setIsOpen] = useState(false)
+
+  //Framer motion variants
+  const circleVariants = {
+    hidden : {
+      scale:0
+    },
+    visible : {
+      scale:180,
+      transition: {
+        type: 'spting',
+        stiffness: 160,
+        damping: 60,
+      }
+    }
+  }
+  const ulVariants = {
+    hidden : {
+      opacity:0
+    },
+    visible : {
+      opacity:1,
+      transition: {
+        delay: 0.1,
+      }
+    }
+  }
   return (
-    <div>NavMobile</div>
+    <nav className='relative'>
+      {/* Menu Items  */}
+      <div className='cursor-pointer text-white '>
+<MenuAlt3Icon className= 'w-8 h-8' />
+      </div>
+
+    </nav>
   )
 }
 
