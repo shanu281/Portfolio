cab007445a569049028b84e58eba817fb0282d43
Shanu Kumar <shanukumar467@gmail.com>
2023-05-20 12:35:48 +0530

    Brands component created

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Header from './Components/Header';
 import Hero from './Components/Hero';
+import Brands from './Components/Brands/Brands';
 
 const App = () => {
   return <div>
     <Header />
     <Hero />
+    <Brands />
     <div style={{height: '2000px'}}>
 
     </div>
