f77436f071cc0f900cf177ccedc17fb50f1b1cdc
Shanu Kumar <shanukumar467@gmail.com>
2023-05-20 13:52:24 +0530

    skills Component added

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Header from './Components/Header';
 import Hero from './Components/Hero';
 import Brands from './Components/Brands';
 import About from './Components/About';
+import Skills from './Components/Skills';
 
 const App = () => {
   return <div>
@@ -10,6 +11,7 @@ const App = () => {
     <Hero />
     <Brands />
     <About />
+    <Skills />
     <div style={{height: '2000px'}}>
 
     </div>
